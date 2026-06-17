4287856d3dc148c3a61bdee42d45ff257f34cfc3
Dante <57098163+aymking1001@users.noreply.github.com>
2026-06-17 02:34:21 +0200

    Update worker.py

--- worker.py
+++ worker.py
@@ -1,1 +1,24 @@
 
+import os
+import base64
+from telethon import TelegramClient
+
+session_data = base64.b64decode(
+    os.environ["TELEGRAM_SESSION"]
+)
+
+with open("github_session.session", "wb") as f:
+    f.write(session_data)
+
+client = TelegramClient(
+    "github_session",
+    int(os.environ["TELEGRAM_API_ID"]),
+    os.environ["TELEGRAM_API_HASH"]
+)
+
+async def main():
+    me = await client.get_me()
+    print(f"Logged in as: {me.id} | {me.first_name}")
+
+with client:
+    client.loop.run_until_complete(main())
